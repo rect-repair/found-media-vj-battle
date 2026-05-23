b5e01ebe0b762800674b32c3b0b367b283a12c09
viola he <viola@nyu.edu>
2026-05-24 01:45:11 +0800

    dynamic p5 bg scaling

--- sketch.js
+++ sketch.js
@@ -21,18 +21,24 @@ function draw() {
     translate(width / 2, height / 2);
     noStroke();
     rotate(rotateDeg);
-    console.log(frameCount % 360)
     for (let a = 0; a < 360; a += 20) {
         push()
         rotate(a);
         let lineCalc = abs(sin(a))
-        let lineW = map(lineCalc, 0, 1, 110, 140)
+        let lineScale = map(width, 200, 1920, 0.6, 2)
+        let lineW = map(lineCalc, 0, 1, lineScale * 110, lineScale * 140)
+        let length = Math.max(width, height);
         fill(40);
-        triangle(0, 0, 0, width, lineW, width)
+        triangle(0, 0, 0, length, lineW, length)
         pop();
     }
     // triangle(0, 0, 0, -height, 30, -height)
     pop();
     // circle(mouseX, mouseY, 10)
 
+}
+
+function windowResized(event) {
+    resizeCanvas(windowWidth, windowHeight);
+    print(event);
 }
