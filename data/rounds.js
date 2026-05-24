ec2e84aad2ebfab07d7b2cba947b7d998a8477c8
viola he <viola@nyu.edu>
2026-05-23 19:48:13 -0700

    add diary for testing, better mobile

--- data/rounds.js
+++ data/rounds.js
@@ -42,57 +42,69 @@ const BATTLE_ROUNDS = [
   {
     round: 5,
     label: "Round 5",
-    red: { name: "yuemin", diary: "…" },
-    blue: { name: "apple", diary: "…" },
+    red: {
+      name: "yuemin", diary:
+        "原初构想：\n视频直接从bilibili首页，知识/体育/科技/游戏里面现找\n找到比较好笑，但跟音乐毫无关系的标题点开靠调视频速度来卡节拍,用不同的放大方法解构画面，但用鼠标引导视线，用现有画面中的所有的文字，图像，甚至桌面元素重新构建自己的叙事。\n- - - 以上是理想状态 - - -\n实际：显然现场完全听歌听High, 忘记了挑选视频这件事情，沉迷在节奏当中无法自拔（叹气 而且！我因为注意力完全被bilibili的“正在缓冲…”带走，还发现静音键很有意思，好像最后的时间都花在：怎么卡上“正在缓冲…”的图标？？？以至于莫名其妙停了很久，sry，我真的后面跟自己玩了起来，不顾观众死活。\n视频选择\n在我原本的方案里。。。视频还是以我内心听到的音乐和当场选择的频道有关系。。所以没有找太多准备（除了稍微训练了一下推荐流）但！事实证明这件事情现场做太极限了hhh\n准备的视频列表：\n现场用到：\n散度与旋度：麦克斯韦方程组、流体等所用到的语言\n谁能在无尽尸潮中存活更久？（开场，有红蓝大战关键词）\n小伙骑行阿里地区，艰难翻过5190那根拉山。。。（没用上）\n日常精神续作！《小城日常》深度解读（没用上）\n谁能在无尽尸潮中存活更久？（开场，有红蓝大战关键词）\n我又相信光了（现找）\n我发明了永动机（现找，莫名其妙用的）\n我真随机找视频。。。所以。。",
+    },
+    blue: {
+      name: "apple", diary:
+        "Theme：Slice 0023\nSource：Youtube\n为了找到搜索浏览量低于300，最好只有20以下甚至无人播放的视频，以0023为锚点，我搜索了诸如：IMG 0023，MOV 0023，test 0023 等未被命名直接上传，仅用于个人存档使用的作品。它们都是未经处理的私人化视频影像，公开但没有传播目的，因此都是全然野生的原始素材，由此作为我的「0023切片」VJ主题。\n搜索结果大多是几年前的作品，可以看到数学课，孩子记录，各种运动记录，个人或者小乐队自己的音乐排练，学校舞台记录，小动物视频。无数人普通而丰富的生活的一瞥。 Pure existence in the digital wilderness.\n\nSearch List:\nIMG 0023\nDSC 0023\nMOV 0023\nMVI 0023\nBandicam 0023 （古老录屏软件）\nretro game 0023\nGameTest 0023\n我将其中有意思的存到我的search 0023 播放列表，并根据视频性质进行了分类。",
+    },
     result: "blue",
   },
-  {
+  { 
     round: 6,
     label: "Round 6",
     red: { name: "Wenwen", diary: "…" },
-    blue: { name: "Archy", diary: "…" },
+blue: { name: "Archy", diary: "…" },
     result: "red",
-  },
+},
   {
-    round: 7,
-    label: "Round 7",
+    
+      round: 7,
+        label: "Round 7",
     red: { name: "tianqi", diary: "…" },
     blue: { name: "sihui", diary: "…" },
     result: "blue",
   },
-  {
-    round: 8,
-    label: "Round 8",
-    red: { name: "EwAn Qian", diary: "…" },
-    blue: { name: "daymoss", diary: "…" },
+    {
+      
+        round: 8,
+          label: "Round 8",
+      red: { name: "EwAn Qian", diary: "…" },
+      blue: { name: "daymoss", diary: "…" },
     result: "red",
-  },
-  {
-    round: 9,
-    label: "Round 9",
-    red: { name: "Liejie", diary: "…" },
-    blue: { name: "Chagbai", diary: "…" },
+    },
+      {
+        
+          round: 9,
+            label: "Round 9",
+        red: { name: "Liejie", diary: "…" },
+        blue: { name: "Chagbai", diary: "…" },
     result: "blue",
-  },
-  {
-    round: 10,
-    label: "Round 10",
-    red: { name: "Seph", diary: "…" },
-    blue: { name: "Kedi", diary: "…" },
+      },
+        {
+          
+            round: 10,
+              label: "Round 10",
+          red: { name: "Seph", diary: "…" },
+          blue: { name: "Kedi", diary: "…" },
     result: "red",
-  },
-  {
-    round: 11,
-    label: "Round 11",
-    red: { name: "kuanfuweimu", diary: "…" },
-    blue: { name: "jingwei", diary: "…" },
+        },
+          {
+            
+              round: 11,
+                label: "Round 11",
+            red: { name: "kuanfuweimu", diary: "…" },
+            blue: { name: "jingwei", diary: "…" },
     result: "red",
-  },
-  {
-    round: 12,
-    label: "Final Round",
-    red: { name: "chen xin", diary: "…" },
-    blue: { name: "joanna", diary: "…" },
+          },
+            {
+              
+                round: 12,
+                  label: "Final Round",
+              red: { name: "chen xin", diary: "…" },
+              blue: { name: "joanna", diary: "…" },
     result: "red",
-  },
-];
+            },
+]; 
